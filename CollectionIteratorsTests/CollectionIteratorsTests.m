0202e70006bc343056ad1f43c3313129ac87fd3a
Mirko Kiefer <mail@mirkokiefer.com>
2011-03-15 11:52:53 +0100

    better tests

--- CollectionIteratorsTests/CollectionIteratorsTests.m
+++ CollectionIteratorsTests/CollectionIteratorsTests.m
@@ -27,32 +27,32 @@
 
 - (void)testForEach
 {
-  NSArray* anArray = [NSArray arrayWithObjects: @"Dog", @"Cat", @"Shark", nil];
+  NSArray* animals = [NSArray arrayWithObjects: @"Dog", @"Lionfish", @"Cat", @"Shark", nil];
   NSMutableArray* resultArray = [NSMutableArray array];
   
-  [anArray forEach:^(id each) {
+  [animals forEach:^(id each) {
     [resultArray addObject:each];
   }];
   
-  STAssertEquals([anArray count], [resultArray count], @"forEach fails");
+  STAssertEquals([animals count], [resultArray count], @"forEach fails");
 }
 
 - (void)testForEachIndexed
 {
-  NSArray* anArray = [NSArray arrayWithObjects: @"Dog", @"Cat", @"Shark", nil];
-  NSMutableArray* resultArray = [NSMutableArray arrayWithObjects: @"a", @"b", @"c", nil];
+  NSArray* animals = [NSArray arrayWithObjects: @"Dog", @"Lionfish", @"Cat", @"Shark", nil];
+  NSMutableArray* resultArray = [NSMutableArray arrayWithObjects: @"a", @"b", @"c", @"d", nil];
   
-  [anArray forEachIndexed:^(id each, NSInteger index) {
+  [animals forEachIndexed:^(id each, NSInteger index) {
     [resultArray replaceObjectAtIndex:index withObject:each];
   }];
   
-  STAssertTrue([resultArray isEqualToArray: anArray], @"forEachIndexed fails");
+  STAssertTrue([resultArray isEqualToArray: animals], @"forEachIndexed fails");
 }
 
 - (void)testFilter
 {
-  NSArray* animals = [NSArray arrayWithObjects: @"Dog", @"Cat", @"Shark", nil];
-  NSArray* fishes = [NSArray arrayWithObjects: @"Shark", nil];
+  NSArray* animals = [NSArray arrayWithObjects: @"Dog", @"Lionfish", @"Cat", @"Shark", nil];
+  NSArray* fishes = [NSArray arrayWithObjects: @"Lionfish", @"Shark", nil];
   
   NSArray* filterFishes = [animals filter: ^(id each) {
     return [fishes containsObject:each];
@@ -63,10 +63,10 @@
 
 - (void)testCollect
 {
-  NSArray* animals = [NSArray arrayWithObjects: @"Dog", @"Cat", @"Shark", nil];
-  NSArray* fishes = [NSArray arrayWithObjects: @"Shark", nil];
+  NSArray* animals = [NSArray arrayWithObjects: @"Dog", @"Lionfish", @"Cat", @"Shark", nil];
+  NSArray* fishes = [NSArray arrayWithObjects: @"Shark", @"Lionfish", nil];
   
-  NSArray* targetArray = [NSArray arrayWithObjects: @"noFish", @"noFish", @"fish", nil];
+  NSArray* targetArray = [NSArray arrayWithObjects: @"noFish", @"fish", @"noFish", @"fish", nil];
   
   NSArray* resultArray = [animals collect: ^(id each) {
     if([fishes containsObject:each]) {
